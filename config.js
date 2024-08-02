7ffc2d1d838a33453c9330efb9d5d33c90acd6bc
TaittoMC <thienmuaallain@gmail.com>
2024-08-02 12:41:23 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Best Friend';
+const giftUrl = 'https://www.facebook.com/Verseau2k9/';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'best Friend';
+const contentCard = 'Thoáng chốc đã đến tháng 20-10, nhân ngày này thì tao mày món quà gián tiếp thay vì món quà trực tiếp ,tất nhiên thì món quà này ko bằng món quà của Hào rồi, trong 3 năm cấp 3 thì sẽ có những khó khăn về kiến thức hay là thi đại học ở lớp 12 , tao chúc mày thành công trong cuộc sống và khỏe mạnh,Tao biết tao nói hơi nhiều rồi , nói chung là HỌC TỐT VỚI THÀNH CÔNG NHA ';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'Tùng Linh.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
